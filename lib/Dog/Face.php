961760be0445c6979393e4084a596553a81bcbc6
Sam Boyer <tech@samboyer.org>
2011-07-10 11:50:50 -0400

    Introduce the notion of init-phase to Dog\Face.
    When dog-init or dog-rollout are run, the Dog\Face has to run in
    semi-crippled mode, as it can't run any of the ordinary checks to make
    sure the dog environment is sane (as it doesn't exist yet).

--- lib/Dog/Face.php
+++ lib/Dog/Face.php
@@ -13,6 +13,23 @@ use Dog\Exception\BadDog;
  * Acts as a broker, a factory, and a handful of other things, too.
  */
 class Face {
+  /**
+   * Boolean flag indicating whether this DogFace is acting in initialization
+   * capacity.
+   *
+   * Initialization encompasses two commands: dog-init and dog-rollout. In these
+   * cases, the DogFace object needs to be instantiated before any repositories
+   * exist on disk, and before a sled exists (for init) or is accessible (for
+   * rollout).
+   *
+   * Some DogFace actions are only available when in an initialization state,
+   * while some others are only available when in a non-initialization state.
+   *
+   * Defaults to FALSE.
+   *
+   * @var bool
+   */
+  protected $init = FALSE;
 
   /**
    *
@@ -23,6 +40,15 @@ class Face {
   protected $path;
 
   /**
+   * A path from which the DogFace should start its search (by climbing upwards
+   * towards root) for a valid Dog instance, as indicated by the presence of a
+   * sled.
+   *
+   * @var string
+   */
+  protected $suggestedPath;
+
+  /**
    * An associative array of objects implementing DogRepositoryInterface.
    * This set of objects represents all repositories known to this Dog instance.
    *
@@ -30,41 +56,59 @@ class Face {
    */
   protected $repositories = array();
 
+  /**
+   * Construct a new Dog\Face object.
+   *
+   * FIXME the flow here is awkward wrt handling the init case.
+   *
+   * @param string $suggested_path
+   * @param bool $init
+   * @return \Dog\Face
+   */
   public function __construct($suggested_path = NULL, $init = FALSE) {
+    $this->init = $init;
     if (TRUE === $init) {
-      // We are init'ing something new. Go with provided path, or use cwd.
-      $this->path = $suggested_path ?: getcwd();
-      return;
+      // We are init'ing something new. Go with provided path, or use cwd, and
+      // skip all other initialization logic.
+      return $this->path = $suggested_path ?: getcwd();
     }
 
     if (NULL === $suggested_path) {
       // No path is provided. Try to suss our way out to the Dog root.
       if (defined('DRUPAL_ROOT')) {
-        $suggested_path = DRUPAL_ROOT;
+        $this->suggestedPath = DRUPAL_ROOT;
       }
       else {
-        $msg = 'No DRUPAL_ROOT, no suggested path, and not setting up a new instance; cannot make a DogFace.';
+        $this->suggestedPath = getcwd();
+//        $msg = 'No DRUPAL_ROOT, no suggested path, and not setting up a new instance; cannot make a DogFace.';
+//        throw new BadDog($msg, E_RECOVERABLE_ERROR);
+      }
+    }
+  }
+
+  /**
+   * Return the root path of the current Dog instance.
+   *
+   * @return string
+   */
+  public function getBasePath() {
+    if (NULL === $this->path) {
+      $found = FALSE;
+      $path = $this->suggestedPath;
+      while (FALSE === $found && '/' !== $path) {
+        if (file_exists($path . '/sled')) {
+          $found = TRUE;
+          $this->path = $path;
+        }
+        $path = dirname($path);
+      }
+
+      if (FALSE === $found) {
+        $msg = sprintf('No valid Dog instance could be found when climbing up towards root from %s.', $suggested_path);
         throw new BadDog($msg, E_RECOVERABLE_ERROR);
       }
     }
 
-    $found = FALSE;
-    $path = $suggested_path;
-    while (FALSE === $found && '/' !== $path) {
-      if (file_exists($path . '/sled')) {
-        $found = TRUE;
-        $this->path = $path;
-      }
-      $path = dirname($path);
-    }
-
-    if (FALSE === $found) {
-      $msg = sprintf('No valid Dog instance could be found when climbing up towards root from %s.', $suggested_path);
-      throw new BadDog($msg, E_RECOVERABLE_ERROR);
-    }
-  }
-
-  public function getBasePath() {
     return $this->path;
   }
 
